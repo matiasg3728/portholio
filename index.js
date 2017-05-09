1e64ad604195d0c41d861d5f550b1f26cc373e14
Matias Garcia <matiasgarcia@Matiass-MacBook-Air.local>
2017-05-08 22:25:12 -0500

    idk why gitignore isnt working

--- index.js
+++ index.js
@@ -1,24 +1,55 @@
-$(document).on('click', '.nav-tabs li', function(){
-	$('.nav-tab-list_ele').removeClass("active");
-	$(this).addClass("active");
-});
 
-//TY @davidtmiller
+/*
+    we can come back to this later
+*/
 
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
 
-$(function() {
-    $(document).on('click', 'a.page-scroll', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
+// AOS.init({
+//  duration: 1200
+// });
+
+// var selector = '.nav li';
+
+// $(selector).on('click', function(){
+//     $(selector).removeClass('active');
+//     $(this).addClass('active');
+// });
+
+// $(window).scroll(function() {
+//     if ($(".navbar").offset().top > 50) {
+//         $(".navbar-fixed-top").addClass("top-nav-collapse");
+//     } else {
+//         $(".navbar-fixed-top").removeClass("top-nav-collapse");
+//     }
+// });
+
+// $(function() {
+//     $(document).on('click', 'a.page-scroll', function(event) {
+//         var $anchor = $(this);
+//         $('html, body').stop().animate({
+//             scrollTop: $($anchor.attr('href')).offset().top
+//         }, 1500, 'easeInOutExpo');
+//         event.preventDefault();
+//     });
+// });
+
+// setCarouselHeight('#info_carousel');
+
+// function setCarouselHeight(id)
+// {
+//     var slideHeight = [];
+//     $(id+' .item').each(function()
+//     {
+//         // add all slide heights to an array
+//         slideHeight.push($(this).height());
+//     });
+
+//     // find the tallest item
+//     max = Math.max.apply(null, slideHeight);
+
+//     // set the slide's height
+//     $(id+' .carousel-content').each(function()
+//     {
+//         $(this).css('height',max+'px');
+//     });
+// }
---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$('#beginer').on('click', function() {
+
+	this.classList.toggle('hide')
+
+
+
+	$('#top_part').addClass('move_up')
+
+	$('#bottom_part').addClass('move_down')
+
+
+})
+
